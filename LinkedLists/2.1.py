b2d508321e5b3d0368cc8e0b008f6f2900297d54
Ian Middelkamp <ianmiddelkamp@gmail.com>
2020-03-05 06:49:16 -0500

    2.2.2

--- LinkedLists/2.1.py
+++ LinkedLists/2.1.py
@@ -18,13 +18,32 @@ def solve_it1(arr):
                sllnew.addNewNode(node.data)          
           node = node.next
      sllnew.printList()
+
+def solve_it2(arr):
+     sll = LL.SingleLinkedList(arr)       
+     node = sll.head
+     while node!= None:   
+          node2 = node
+          while node2.next!=None:            
+               if node2.next.data == node.data:
+                    node2.next = node2.next.next            
+               else:
+                    node2 = node2.next
+          node = node.next
+     sll.printList()
+
 import sys
 
 if __name__ == '__main__':
+
      start = time.perf_counter()
-     arr = [1,5,6,8,3,5,8,9,10,3, 5, 99]
-     
+     arr = [1,5,6,8,3,5,8,9,10,3, 5, 99]     
      solve_it1(arr)
      end = time.perf_counter()
+     print('Total Time:\n' + str(end - start)) 
 
+     start = time.perf_counter()
+     arr = [1,5,6,8,3,5,8,9,10,3, 5, 99]
+     solve_it2(arr)
+     end = time.perf_counter()
      print('Total Time:\n' + str(end - start))
